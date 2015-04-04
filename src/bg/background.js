9b997e325e5323cb9b33e9477f0000e5e37b0fb4
vjcho <vjcho@ucdavis.edu>
2015-04-04 04:32:50 -0700

    button opens it in new page instead of popup

--- src/bg/background.js
+++ src/bg/background.js
@@ -6,8 +6,16 @@
 
 
 //example of using a message handler from the inject scripts
+
+/*
 chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
   	chrome.pageAction.show(sender.tab.id);
     sendResponse();
   });
+*/
+
+chrome.browserAction.onClicked.addListener(function(activeTab){
+  var newURL = chrome.extension.getURL("src/browser_action/browser_action.html");
+  chrome.tabs.create({ url: newURL });
+});
