e709b59daa0aba7277d1a9615da98adb00041ce3
Richard Min <richardmin97@g.ucla.edu>
2015-04-04 04:55:05 -0700

    Updated manifest; updated browser_action to more accurately reflect the database structure

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -1,14 +1,14 @@
 Firebase.enableLogging(true);
 var f = new Firebase('https://vivid-heat-3174.firebaseio.com/');
-
-f.transaction(function(curr) {
+var fc = f.child('clicks');
+fc.transaction(function(curr) {
   if (isNaN(parseFloat(curr)))
     return 1; // initialize to 1.
   else
     return curr + 1; // increment.
 }, function() {
     // Once the transaction has completed, update the UI (and watch for updates).
-    f.on('value', function(s) {
+    fc.on('value', function(s) {
       document.getElementById('contents').innerHTML = s.val();
     });
   });
