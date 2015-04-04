65616cfc2fdcfe75efef5d56afde2672dfa67379
Richard Min <richardmin97@g.ucla.edu>
2015-04-04 04:18:09 -0700

    user_manager updated, with objects named more appropiately

--- src/user_management/user_manager.js
+++ src/user_management/user_manager.js
@@ -1,9 +1,6 @@
 var ref = new Firebase("https://vivid-heat-3174.firebaseio.com");
 
-ref.createUser({
-  email: "bobtony@firebase.com",
-  password: "correcthorsebatterystaple"
-}, function(error, userData) {
+ref.createUser(emailPassWordObject, function(error, userData) {
   if (error) {
     switch (error.code) {
       case "EMAIL_TAKEN":
@@ -17,5 +14,44 @@ ref.createUser({
     }
   } else {
     console.log("Successfully created user account with uid:", userData.uid);
+  }
+});
+
+ref.authWithPassword(emailPassWordObject, function(error, authData) {
+  if (error) {
+    console.log("Login Failed!", error);
+  } else {
+    console.log("Authenticated successfully with payload:", authData);
+  }
+});
+
+ref.changePassword(emailOldNewPassWordObject, function(error) {
+  if (error) {
+    switch (error.code) {
+      case "INVALID_PASSWORD":
+        console.log("The specified user account password is incorrect.");
+        break;
+      case "INVALID_USER":
+        console.log("The specified user account does not exist.");
+        break;
+      default:
+        console.log("Error changing password:", error);
+    }
+  } else {
+    console.log("User password changed successfully!");
+  }
+});
+
+ref.resetPassword(emailObject, function(error) {
+  if (error) {
+    switch (error.code) {
+      case "INVALID_USER":
+        console.log("The specified user account does not exist.");
+        break;
+      default:
+        console.log("Error resetting password:", error);
+    }
+  } else {
+    console.log("Password reset email sent successfully!");
   }
 });
